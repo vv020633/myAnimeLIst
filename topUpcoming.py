e1006d4493b433112855a493afc635fb5854263d
chiefchas <chas.mclaughlin1@gmail.com>
2020-03-29 04:56:54 +0100

    Functionality added to allow user to search for upcoming titles on reddit

--- topUpcoming.py
+++ topUpcoming.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 
-import json, requests, pprint, os, time, math
+import json, requests, pprint, os, time, math, webbrowser, re
 from jikanpy import Jikan
 jikan = Jikan()
 
@@ -35,7 +35,7 @@ def jsonToMenu():
 #Initates the main menu
 def mainMenu(titles, ranks, start_dates):
 
-    while 1:
+    while True:
         #Clear terminal command for Windows, Unix and MAC
         os.system('cls' if os.name == 'nt' else 'clear')
         print("Hello. Please select 1 to view upcoming anime.\n \n" )
@@ -50,9 +50,59 @@ def mainMenu(titles, ranks, start_dates):
             time.sleep(2)
 
     time.sleep(1)
-    upcomingMenuPrint(titles, ranks, start_dates)
+    user_input = upcomingMenuPrint(titles, ranks, start_dates)
+    return user_input
+
+# Selects one of the upcoming anime titles
+def titleSelect(titles, ranks, user_input):
+    # Searches for the selected title on reddit using our search token
+    def redditSearch(search_token):
+        search_link = f'https://www.reddit.com/r/anime/search/?q={search_token}&restrict_sr=1'
+        try:
+            webbrowser.open(search_link)
+
+        except ConnectionError:
+            print(f'Could not connect to destination: {search_link}' )
+
+    #Clear terminal command for Windows, Unix and MAC
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+    while True:
+        for rank in ranks:
+            # If user input is a digit
+            if user_input.isdigit():
+                # If the user input matches a value in our ranks
+                # I'll have to come back to this as this should be limited to 10
+                if int(user_input) == rank:
+                    search_string = titles[rank-1]
+                    # If there's white space in the string then we're going to join the string using '%20' as the delimeter
+                    # WHich is reddit's method of appending search string values
+                    if ' ' in search_string:
+                        search_array = search_string.split()
+                        search_token = '%20'.join(search_array)
+                        redditSearch(search_token)
+                        break
+                    else:
+                        redditSearch(search_string)
+                        break
+                else:
+                    continue
+            # Go back if the user's selection is 'B'
+            elif str(user_input).upper() == 'B':
+                        mainMenu(titles, ranks, start_dates)
+
+            else:
+                continue
+
+        break
+        time.sleep(1)
+        print('**************Invalid selection**************')
+        time.sleep(5)
+        upcomingMenuPrint(titles, ranks, start_dates)
+
 def upcomingMenuPrint(titles, ranks, start_dates):
-# assigns the max length of the columns for each value
+# assigns the max width of the columns for each value
     max_length_titles = max(titles, key = len)
     max_length_ranks = 2
     max_length_start_dates = max(start_dates, key = len)
@@ -60,34 +110,23 @@ def upcomingMenuPrint(titles, ranks, start_dates):
     max_titles_col_length = len(max_length_titles)
     max_ranks_col_length = max_length_ranks
     max_start_dates_col_length = len(max_length_start_dates)
+# Column title fields
+    print('Rank'.ljust(max_ranks_col_length, '-')  + 'Title'.center(max_titles_col_length, ' ') +  'Start Date'.rjust(max_start_dates_col_length, ' ') + '\n')
 
-    if int(max_titles_col_length) % 2 != 0 :
-        max_titles_col_length = max_titles_col_length + 1
+    for count in range(0,20):
+# Prints the values of the top Upcoming anime
+        print(str(ranks[count]).center(10, ' ') + '''[''' + str(titles[count]).center(70, '+') + ''']'''  + str(start_dates[count]).rjust(20, ' ') + '\n' + '\n')
 
-    if int(max_start_dates_col_length) % 2  != 0 :
-        max_start_dates_col_length = max_start_dates_col_length + 1
-
-    max_titles_col_length = max_titles_col_length + 2
-    max_ranks_col_length = max_ranks_col_length + 2
-    max_start_dates_col_length = max_start_dates_col_length + 2
-
-
-    print('Rank'.ljust(max_ranks_col_length, '-')  + 'Title'.center(max_titles_col_length, ' ') + '\t'+ 'Start Date'.rjust(max_start_dates_col_length, ' ') + '\n')
-
-    for count in range(0,10):
-
-        title_justify = int(max_titles_col_length -len(titles[count])/2)
-        ranks_justify = 8
-        title_justify = title_justify+ranks_justify
-        start_dates_justify = int(max_start_dates_col_length -len(start_dates[count])/2)
-        start_dates_justify = (max_titles_col_length - title_justify) + start_dates_justify
-        # titles_col_width = (max_length_titles - len(titles[count])) + 1
-        # titles_col_width = (max_length_ranks - len(ranks[count])) + 1
-        # titles_col_width = (max_length_start_dates - len(start_dates[count])) + 1
-        print(str(ranks[count]).center(ranks_justify, ' ') + '''[''' + str(titles[count]).center(title_justify, '+') + ''']'''  + str(start_dates[count]).rjust(start_dates_justify, ' ') + '\n')
-
-
-    # return len(max_length)
+    time.sleep(.2)
+    print('Please input a rank number from the list for more information regarding a chosen title...')
+    time.sleep(.2)
+    print("Press 'B' to go back...")
+    user_input = input()
+    return user_input
+# titleSelect(titles, ranks, user_input)
+# Run title select function here
+# If value is found then break and clear Screen
+# If value is not found, then keep printing to the screen
 
 
 # Grabbing the top upcoming anime from the MAL API
@@ -101,5 +140,5 @@ except ConnectionError:
 
 
 titles, ranks, start_dates = jsonToMenu()
-print(start_dates)
-mainMenu(titles, ranks, start_dates)
+user_input = mainMenu(titles, ranks, start_dates)
+titleSelect(titles, ranks, user_input)
