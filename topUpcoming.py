257b11cbe2e6a668951e01486ce8d58996df3191
chiefchas <chas.mclaughlin1@gmail.com>
2020-03-29 21:43:21 +0100

    Changes added to main menu to allow user to select random anime based on genre + changes made to functioncall workflow

--- topUpcoming.py
+++ topUpcoming.py
@@ -1,12 +1,28 @@
 #! /usr/bin/python3
 
-import json, requests, pprint, os, time, math, webbrowser, re
+import json, requests, pprint, os, time, math, webbrowser, re, pyautogui
 from jikanpy import Jikan
 jikan = Jikan()
 
+def jsonToGenreMenu():
+    try:
+        # Grabbing the top upcoming anime from the MAL API
+        anime_genre = jikan.genre(type='anime', genre_id=1)
+
+    except ConnectionError:
+        print('Could not connect to the MAL API at: https://api.jikan.moe/v3')
+
+    pprint.pprint(anime_genre)
 
 # Loops through the retrieved Json values and pulls the 'top' dictionary item from the dictionary which contains a list of animes as it's value
-def jsonToMenu():
+def jsonToUpcomingMenu():
+    try:
+        # Grabbing the top upcoming anime from the MAL API
+        top_anime = jikan.top(type='anime', page=1, subtype='upcoming')
+
+    except ConnectionError:
+        print('Could not connect to the MAL API at: https://api.jikan.moe/v3')
+
     titles = []
     ranks = []
     start_dates = []
@@ -33,25 +49,31 @@ def jsonToMenu():
     return titles, ranks, start_dates
 
 #Initates the main menu
-def mainMenu(titles, ranks, start_dates):
+def mainMenu():
 
     while True:
         #Clear terminal command for Windows, Unix and MAC
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("Hello. Please select 1 to view upcoming anime.\n \n" )
+        print('[1]View Top Upcoming Anime.'.center(20) + '\n ' )
+        print('[2]Select Anime to Watch by Genre.'.center(20) + '\n ' )
 #If the user's input isn't an integer then it raises an exception
         try:
             menu_option = int(input())
-            break
+            if menu_option == 1:
+
+                titles, ranks, start_dates = jsonToUpcomingMenu()
+                upcomingMenu(titles, ranks, start_dates)
+                break
+            elif menu_option == 2:
+                jsonToGenreMenu()
+                break
         except ValueError:
             print("Error")
             time.sleep(1)
             print("Please enter a digit...")
             time.sleep(2)
 
-    time.sleep(1)
-    user_input = upcomingMenuPrint(titles, ranks, start_dates)
-    return user_input
+
 
 # Selects one of the upcoming anime titles
 def titleSelect(titles, ranks, user_input):
@@ -63,6 +85,25 @@ def titleSelect(titles, ranks, user_input):
 
         except ConnectionError:
             print(f'Could not connect to destination: {search_link}' )
+
+# Searches for the selected title on Youtube using our search token
+    def youtubeSearch(search_token):
+        search_link = f'https://www.youtube.com/results?search_query={search_token}'
+        try:
+            webbrowser.open_new_tab(search_link)
+
+        except ConnectionError:
+            print(f'Could not connect to destination: {search_link}' )
+
+
+# Searches for the selected title on Wikipedia using our search token
+    def wikipediaSearch(search_token):
+        search_link = f'https://en.wikipedia.org/wiki/{search_token}'
+        try:
+            webbrowser.open_new_tab(search_link)
+        except ConnectionError:
+            print(f'Could not connect to destination: {search_link}' )
+
 
     #Clear terminal command for Windows, Unix and MAC
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -77,14 +118,22 @@ def titleSelect(titles, ranks, user_input):
                 if int(user_input) == rank:
                     search_string = titles[rank-1]
                     # If there's white space in the string then we're going to join the string using '%20' as the delimeter
-                    # WHich is reddit's method of appending search string values
+                    # Which is reddit's method of appending search string values
                     if ' ' in search_string:
                         search_array = search_string.split()
-                        search_token = '%20'.join(search_array)
-                        redditSearch(search_token)
+                        reddit_search_token = '%20'.join(search_array)
+                        wikipedia_search_token = '_'.join(search_array)
+                        youtube_search_token = '+'.join(search_array)
+
+                        redditSearch(reddit_search_token)
+                        wikipediaSearch(wikipedia_search_token)
+                        youtubeSearch(youtube_search_token)
+
                         break
                     else:
                         redditSearch(search_string)
+                        wikipediaSearch(search_string)
+                        youtubeSearch(search_string)
                         break
                 else:
                     continue
@@ -99,9 +148,9 @@ def titleSelect(titles, ranks, user_input):
         time.sleep(1)
         print('**************Invalid selection**************')
         time.sleep(5)
-        upcomingMenuPrint(titles, ranks, start_dates)
+        upcomingMenu(titles, ranks, start_dates)
 
-def upcomingMenuPrint(titles, ranks, start_dates):
+def upcomingMenu(titles, ranks, start_dates):
 # assigns the max width of the columns for each value
     max_length_titles = max(titles, key = len)
     max_length_ranks = 2
@@ -122,19 +171,16 @@ def upcomingMenuPrint(titles, ranks, start_dates):
     time.sleep(.2)
     print("Press 'B' to go back...")
     user_input = input()
-    return user_input
-
-
-# Grabbing the top upcoming anime from the MAL API
-try:
-
-    top_anime = jikan.top(type='anime', page=1, subtype='upcoming')
-
-except ConnectionError:
-    print('Could not connect to the MAL API.')
+    titleSelect(titles, ranks, user_input)
 
 
 
-titles, ranks, start_dates = jsonToMenu()
-user_input = mainMenu(titles, ranks, start_dates)
-titleSelect(titles, ranks, user_input)
+def main() :
+
+    mainMenu()
+
+
+
+
+
+main()
